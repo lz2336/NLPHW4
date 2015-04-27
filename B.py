410f1bf4f9fbc195d44e7db0cf6ba6a47d9596f0
Lingzi Zhuang <Lingzi@dyn-160-39-149-30.dyn.columbia.edu>
2015-04-27 18:26:21 -0400

    *

--- B.py
+++ B.py
@@ -224,7 +224,7 @@ class BerkeleyAligner():
                     if (e_word, g_word) in t_eg:
                         t[(g_word, e_word)] = (c_eg[(e_word, g_word)] + c_ge[(g_word, e_word)]) / (c_eg[g_word] + c_ge[e_word])
                         # q[(i, j, l_g, l_e)] = (c_eg[(j, i, l_e, l_g)] + c_ge[(i, j, l_g, l_e)]) / (c_eg[(i, l_e, l_g)] + c_ge[(j, l_g, l_e)])
-                        qq[(i, j, l_g, l_e)] = q_ge[(i, j, l_g, l_e)]
+                        q[(i, j, l_g, l_e)] = q_ge[(i, j, l_g, l_e)]
                     else:
                         t[(g_word, e_word)] = t_ge[(g_word, e_word)]
                         q[(i, j, l_g, l_e)] = q_ge[(i, j, l_g, l_e)]
