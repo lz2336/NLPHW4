6747f7190cc6d6443891d8205c020049c411c882
Lingzi Zhuang <Lingzi@dyn-160-39-149-30.dyn.columbia.edu>
2015-04-27 19:21:22 -0400

    *

--- A.py
+++ A.py
@@ -3,7 +3,7 @@ import codecs
 # from nltk.corpus import comtrans
 from nltk.align import ibm1, ibm2
 
-NUM_ITERS = 6
+NUM_ITERS = 7
 
 # TODO: Initialize IBM Model 1 and return the model.
 def create_ibm1(aligned_sents):
