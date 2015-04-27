0a739ac952ed3d5fb4627373eddf1ebc4e04ae4d
Lingzi Zhuang <Lingzi@dyn-160-39-149-30.dyn.columbia.edu>
2015-04-27 15:38:30 -0400

    *

--- B.py
+++ B.py
@@ -13,21 +13,18 @@ class BerkeleyAligner():
     # TODO: Computes the alignments for align_sent, using this model's parameters. Return
     #       an AlignedSent object, with the sentence pair and the alignments computed.
     def align(self, align_sent):
-        print self.t
-        print self.q
         alignments = []
         german = align_sent.words
         english = [None] + align_sent.mots
         l_g = len(german)
         l_e = len(english) - 1
-
-        p_max = 0.0
+        print 'got here'
 
         for j in range(1, l_g + 1):
             g_word = german[j - 1]
             p_max = (self.t[(g_word, None)] * self.q[(0, j, l_g, l_e)], None)
-            # print self.t[(g_word, None)]
-            # print self.q[(0, j, l_g, l_e)]
+            print self.t[(g_word, None)]
+            print self.q[(0, j, l_g, l_e)]
 
             for i in range (1, l_e + 1):
                 e_word = english[i]
