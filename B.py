493534829c92765fcd885cf803c374c33a8f140d
Lingzi Zhuang <Lingzi@dyn-160-39-149-30.dyn.columbia.edu>
2015-04-27 13:22:07 -0400

    *

--- B.py
+++ B.py
@@ -141,7 +141,7 @@ class BerkeleyAligner():
             g_vocab.add(None)
             for e in e_vocab:
                 for g in g_vocab:
-                    print (g, c_eg[g])
+                    # print (g, c_eg[g])
                     t_eg[(e, g)] = c_eg[(e, g)] / c_eg[g]
             g_vocab.remove(None)
 
